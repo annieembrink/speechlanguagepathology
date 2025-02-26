ea353a3886c8b9e57d79f201cd02645ed5413958
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2025-02-26 21:12:18 +0100

    added text we offer

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TextHomePage from "./textHomePage";
+import TextWeOffer from "./textWeOffer";
 
 const Menu = () => {
     const [activePage, setActivePage] = useState("Hem");
@@ -12,7 +13,7 @@ const Menu = () => {
         <div>
             <div id="menuBar">
                 <button onClick={() => handleClick("Hem")}>Hem</button>
-                <button onClick={() => handleClick("Om")}>Om</button>
+                {/* <button onClick={() => handleClick("Om")}>Om</button> */}
                 <button onClick={() => handleClick("Digital vård")}>Digital vård</button>
                 <button onClick={() => handleClick("Vi erbjuder")}>Vi erbjuder</button>
                 <button onClick={() => handleClick("Priser")}>Priser</button>
@@ -21,9 +22,9 @@ const Menu = () => {
 
             <div id="content">
                 {activePage === "Hem" && <TextHomePage></TextHomePage>}
-                {activePage === "Om" && <p>Om oss: Här är lite information om oss.</p>}
+                {/* {activePage === "Om" && <p>Om oss: Här är lite information om oss.</p>} */}
                 {activePage === "Digital vård" && <p>Vi erbjuder digital vård för alla!</p>}
-                {activePage === "Vi erbjuder" && <p>Här är våra tjänster.</p>}
+                {activePage === "Vi erbjuder" && <TextWeOffer></TextWeOffer>}
                 {activePage === "Priser" && <p>Här hittar du våra priser.</p>}
                 {activePage === "Kontakt" && <p>Kontakta oss gärna!</p>}
             </div>
--- src/components/textHomePage.jsx
+++ src/components/textHomePage.jsx
@@ -7,7 +7,9 @@ const TextHomePage = () => {
         </div>
         
         <div>
-        <p>Jag heter Annie och är legitimerad logoped sedan 2016. Med erfarenhet inom  läs- och skrivsvårigheter samt kommunikation och sväljning hos vuxna, erbjuder jag individuellt anpassad logopedisk hjälp i privat regi.  
+        <p>Jag heter Annie och är legitimerad logoped sedan 2016. Med erfarenhet inom  läs- och skrivsvårigheter samt kommunikation och sväljning hos vuxna, erbjuder jag individuellt anpassad logopedisk hjälp i privat regi.</p></div>
+        
+        <div><p>
         Eftersom verksamheten ej har avtal med landstinget, innebär det att du som patient själv står för kostnaden. Fördelen är kortare väntetider, större flexibilitet och en mer personlig vårdupplevelse där vi tillsammans skapar en plan utifrån dina behov och önskemål.  Du behöver alltså ingen remiss för att komma till oss.</p>
         </div>
         
