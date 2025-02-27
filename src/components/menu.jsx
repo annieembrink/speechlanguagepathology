512fbd1c6eb678faf4d3e651b7c750b11f98f7bd
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2025-02-27 08:40:40 +0100

    push before major changes

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,32 +1,12 @@
-import { useState } from "react";
-import TextHomePage from "./textHomePage";
-import TextWeOffer from "./textWeOffer";
-
-const Menu = () => {
-    const [activePage, setActivePage] = useState("Hem");
-
-    const handleClick = (page) => {
-        setActivePage(page);
-    };
-
+const Menu = ({ setActivePage }) => {
     return ( 
         <div>
             <div id="menuBar">
-                <button onClick={() => handleClick("Hem")}>Hem</button>
-                {/* <button onClick={() => handleClick("Om")}>Om</button> */}
-                <button onClick={() => handleClick("Digital vård")}>Digital vård</button>
-                <button onClick={() => handleClick("Vi erbjuder")}>Vi erbjuder</button>
-                <button onClick={() => handleClick("Priser")}>Priser</button>
-                <button onClick={() => handleClick("Kontakt")}>Kontakt</button>
-            </div>
-
-            <div id="content">
-                {activePage === "Hem" && <TextHomePage></TextHomePage>}
-                {/* {activePage === "Om" && <p>Om oss: Här är lite information om oss.</p>} */}
-                {activePage === "Digital vård" && <p>Vi erbjuder digital vård för alla!</p>}
-                {activePage === "Vi erbjuder" && <TextWeOffer></TextWeOffer>}
-                {activePage === "Priser" && <p>Här hittar du våra priser.</p>}
-                {activePage === "Kontakt" && <p>Kontakta oss gärna!</p>}
+                <button onClick={() => setActivePage("Hem")}>Hem</button>
+                <button onClick={() => setActivePage("Digital vård")}>Digital vård</button>
+                <button onClick={() => setActivePage("Vi erbjuder")}>Vi erbjuder</button>
+                <button onClick={() => setActivePage("Priser")}>Priser</button>
+                <button onClick={() => setActivePage("Kontakt")}>Kontakt</button>
             </div>
         </div>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,33 +1,46 @@
-import '../style.css';  // Relativ sökväg från HomePage.js
+import { useState } from "react";
+import '../style.css';
 import Menu from '../components/menu';
 import TextHomePage from '../components/textHomePage';
+import TextWeOffer from '../components/textWeOffer';
 
 const HomePage = () => {
-    return ( 
-<div>
-        <div id="headerContainer">
-            <div id="headerName">
-                <div id='imgDiv'>
-            {/* <img src="/img/bytesize--book.svg" alt="" id='headerImg'/> */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 21 21"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M18.5 6.59q-2-1.09-4-1.09c-2 0-2.667.364-4 1.09v9.91q2-1 4-1c2 0 2.667.333 4 1zm-8 0q-2-1.09-4-1.09c-2 0-2.667.364-4 1.09v9.91q2-1 4-1c2 0 2.667.333 4 1z" stroke-width="0.5"/></svg>
+    const [activePage, setActivePage] = useState("Hem");
 
+    return ( 
+        <div>
+            <div id="headerContainer">
+                <div id="headerName" onClick={() => setActivePage("Hem")}>  
+                    <div id='imgDiv'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 21 21">
+                            <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M18.5 6.59q-2-1.09-4-1.09c-2 0-2.667.364-4 1.09v9.91q2-1 4-1c2 0 2.667.333 4 1zm-8 0q-2-1.09-4-1.09c-2 0-2.667.364-4 1.09v9.91q2-1 4-1c2 0 2.667.333 4 1z" strokeWidth="0.5"/>
+                        </svg>
+                    </div>
+                    <h1 id='headerH1'>Folklogopedi</h1>
+                </div>
+
+                <div id='rightElements'>
+                    <div id="headerTagline">
+                        <h3>Din dyslexilogoped över hela Sverige</h3>
+                    </div>
+                    <div id="headerContact">
+                        <h4>072 538 48 17</h4>
+                        <h4>folklogopedi@gmail.com</h4>
+                    </div>
+                </div>
             </div>
-                <h1 id='headerH1'>Folklogopedi</h1>
-            </div>
-            <div id='rightElements'>
-            <div id="headerTagline">
-                <img src="" alt="" />
-                <h3>Din dyslexilogoped över hela Sverige</h3>
-            </div>
-            <div id="headerContact">
-                <h4>072 538 48 17</h4>
-                <h4>folklogopedi@gmail.com</h4>
-            </div>
+
+            <Menu setActivePage={setActivePage} /> 
+
+            <div id="content">
+                {activePage === "Hem" && <TextHomePage />}
+                {activePage === "Digital vård" && <p>Vi erbjuder digital vård för alla!</p>}
+                {activePage === "Vi erbjuder" && <TextWeOffer />}
+                {activePage === "Priser" && <p>Här hittar du våra priser.</p>}
+                {activePage === "Kontakt" && <p>Kontakta oss gärna!</p>}
             </div>
         </div>
-        <Menu></Menu>
-        </div>
-     );
+    );
 }
- 
+
 export default HomePage;
